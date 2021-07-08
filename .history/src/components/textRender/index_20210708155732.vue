920825ef75a236d63cc50abc20a1ec585e558a14
owen <xiachao_neuq@163.com>
2021-07-08 15:58:42 +0800

    正文内容渲染组件编写

--- .history/src/components/textRender/index_20210708155732.vue
+++ .history/src/components/textRender/index_20210708155732.vue
@@ -88,7 +88,7 @@ export default {
                 date:'2021-07-07',
                 title:'常见的Linux命令',
                 synopsis:'作为前端程序员,合适的飞机事故市国土环境负荷是否合伙人而恶团伙的话更好的的很好的话地方很多和',
-                articleType:'nodejs'
+                articleType:'linux'
             },
         ]
     };
